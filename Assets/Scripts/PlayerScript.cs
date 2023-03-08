851749a014e146fcdce583e2d0fa72cc31d6c00b
Kyle Amburgey <kyle.amburgey97@gmail.com>
2023-03-07 23:10:33 -0500

    UpdateFinal

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
     // Public float speed adds speed multiplier variable to game object
     private Rigidbody2D rd2d;
     public float speed;
+    public Text score;
+    private int scoreValue = 0; // initial score value
 
     // Start is called before the first frame update
     // rd2d finds and saves Rigidbody2D component
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
+        score.text = scoreValue.ToString(); //automatically updates score to 0 on very first frame
     }
 
     // Update is called once per frame
@@ -24,5 +28,26 @@ public class PlayerScript : MonoBehaviour
         float vertMovement = Input.GetAxis("Vertical");
 
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Coin")
+        {
+            scoreValue += 1; // Updates score value by 1
+            score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground") // Checks to see if player is colliding with ground
+        {
+            if(Input.GetKey(KeyCode.W)) // Checks for key being pressed, in this case "W"
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse); // Sudden impluse, then gravity takes effect, force of 3 to verticle, simulates jump
+            }
+        }
     }
 }
